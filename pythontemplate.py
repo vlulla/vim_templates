e39d6fb2754f0ef4794594e908721d5f546d60c7
Vijay Lulla <vijaylulla@gmail.com>
2020-10-12 15:56:03 -0400

    Added some idiomatic example code in python template.

--- pythontemplate.py
+++ pythontemplate.py
@@ -7,12 +7,20 @@ import re,os,sys,sqlite3,datetime
 import numpy as np, pandas as pd
 import matplotlib.pyplot as plt
 
-## import geopandas as gp
+## import geopandas as gp, hypothesis as hp
 
 ## import arcpy as ap
 
+def f1(**kwargs):
+  print("key\tvalue")
+  print("---\t-----")
+  for k,v in kwargs.items():
+    print(f"{k}\t{v}")
+
 def main():
     print("hello world!")
+    f1(fname="vijay",lname="lulla",addr="mythical city",salary=5.2)
+    f1(x=[1,2,3,4,5],y=[1,2,3,4,5])
 
 
 if __name__ == "__main__":
