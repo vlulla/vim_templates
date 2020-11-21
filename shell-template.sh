97c8d62c619a4861db70d88ed1f1c2f3de9d48f9
Vijay Lulla <vijaylulla@gmail.com>
2020-11-21 15:58:03 -0500

    Note on how to use traditional IFS if there arises a need for it.

--- shell-template.sh
+++ shell-template.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euo pipefail
+traditionalIFS="${IFS}"
 IFS=$'\n\t'
 
 ## http://redsymbol.net/articles/unofficial-bash-strict-mode/ 
 ## explains why we need above lines
 
+## If some program needs IFS to be set to traditional value then
+## you can just do IFS="${traditionalIFS}" and then run your program.
 
