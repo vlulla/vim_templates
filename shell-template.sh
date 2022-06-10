40da360644517a182e7949369ffb82b0519d8828
Vijay Lulla <vijaylulla@gmail.com>
2022-06-10 10:33:49 -0400

    Make tmpdir naming, and creation more portable.
    Instead of relying on the -p switch (gnu extension) explicitly create
    the tmpdir in the /tmp directory! Also, use the basename for naming the
    tmpdir! Got this idea from the examples listed in the `man mktemp` on mac!

--- shell-template.sh
+++ shell-template.sh
@@ -9,12 +9,13 @@ IFS=$'\n\t'
 ## If some program needs IFS to be set to traditional value then
 ## you can just do IFS="${traditionalIFS}" and then run your program.
 
-tmpdir=$(mktemp -p /tmp -d tmp.VL.$$.XXXXXXXXXX)
+tmpdir=$(mktemp -d /tmp/tmp.VL.$(basename $0).XXXXXXXXXX)
 cleanup() {
   rm -rf "${tmpdir}"
 }
 trap cleanup EXIT QUIT INT
 
 ## Now you can read/write files into this tmpdir and when the script ends it will 
-## cleanup the temporary directory.
+## cleanup the temporary directory. Or you can do pushd...
+## pushd ${tmpdir}
 
