e68ed57e25052b5f29c0feb8668a78d3bce727d3
Vijay Lulla <vijaylulla@gmail.com>
2022-06-10 20:37:25 -0400

    Go supports complex numbers.

--- gotemplate.go
+++ gotemplate.go
@@ -5,12 +5,12 @@ import (
     // "log"
 )
 
-func f1() (int,string) {
+func f1() (int,string,complex128) {
     fmt.Println("Hello world")
-    return 4_2, "Don't Panic"
+    return 4_2, "Don't Panic",complex(15,2)
 }
 
 func main() {
-    l,m := f1()
-    fmt.Printf("%v => %v\n", l, m)
+    l,m,c := f1()
+    fmt.Printf("%v => %v & %#v\n", l, m, c)
 }
