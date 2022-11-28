0e7d694d217839bbc79f63a4e32053d0109539f2
Vijay Lulla <vijaylulla@gmail.com>
2022-11-27 22:48:09 -0500

    Quoting based on shellcheck recommendations.

--- shell-template.sh
+++ shell-template.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-traditionalIFS="${IFS}"
+## traditionalIFS="${IFS}"
 IFS=$'\n\t'
 
 ## TRACE=1 ./script.sh to enable debug mode!
@@ -12,7 +12,7 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 ## If some program needs IFS to be set to traditional value then
 ## you can just do IFS="${traditionalIFS}" and then run your program.
 
-tmpdir=$(mktemp -d /tmp/tmp.VL.$(basename $0).XXXXXXXXXX)
+tmpdir=$(mktemp -d /tmp/tmp.VL."$(basename "$0")".XXXXXXXXXX)
 cleanup() {
   rm -rf "${tmpdir}"
 }
