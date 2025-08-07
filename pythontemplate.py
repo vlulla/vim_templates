5a4b35cafb396dbf48fac73c34d421f1d2219fa1
Vijay Lulla <vijay_lulla@sweetwater.com>
2025-08-07 09:42:25 -0400

    Selective formatting of file generated by ruff format
    I arrange code formatting to help me see the pattern[s] in the blocks of code and use as much horizontal space as i can.  I find that short lines of code make discerning the overall patterns harder...so not accepting all the formatting changes made by ruff format command.

--- pythontemplate.py
+++ pythontemplate.py
@@ -23,16 +23,25 @@ if __debug__: import pdb
 T = typing.TypeVar("T")
 P = typing.ParamSpec("P")
 
-logging.basicConfig(filename=f"{__file__}-{str(datetime.datetime.now().date())}.log", level=logging.DEBUG, encoding="utf-8", format="{asctime} - {levelname} - {message!r}", style="{",  datefmt="%Y.%m.%dT%H:%M:%S%z")
+logging.basicConfig(
+    filename=f"{__file__}-{str(datetime.datetime.now().date())}.log",
+    level=logging.DEBUG,
+    encoding="utf-8",
+    format="{asctime} - {levelname} - {message!r}",
+    style="{",
+    datefmt="%Y.%m.%dT%H:%M:%S%z",
+)
 
-def grid(axis:typing.Literal["both","x","y"]="both"):
+
+def grid(axis: typing.Literal["both", "x", "y"] = "both"):
     "Show the grid...kinda like graph paper"
     import matplotlib.pyplot as plt
     plt.minorticks_on()
     plt.grid(which="major", linestyle="-", alpha=0.75, axis=axis)
     plt.grid(which="minor", linestyle=":", alpha=0.5, axis=axis)
 
-def log(func: typing.Callable[P,T]) -> typing.Callable[P,T]:
+
+def log(func: typing.Callable[P, T]) -> typing.Callable[P, T]:
     @functools.wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
         signature = ", ".join([f"{a!r}" for a in args] + [f"{k}={v!r}" for k,v in kwargs.items()])
@@ -46,7 +55,8 @@ def log(func: typing.Callable[P,T]) -> typing.Callable[P,T]:
             raise e
     return wrapper
 
-def timefunc(func: typing.Callable[P,T]) -> typing.Callable[P,T]:
+
+def timefunc(func: typing.Callable[P, T]) -> typing.Callable[P, T]:
     """
     Log how long it takes to run the function.
     >>> @timefunc
@@ -59,6 +69,7 @@ def timefunc(func: typing.Callable[P,T]) -> typing.Callable[P,T]:
 
     NOTE: If logging does not show up (usually the case in ipython), call `logging.basicConfig()` in the repl to initialize logging.
     """
+
     @functools.wraps(func)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
         logger = logging.getLogger("timefunc")
@@ -66,7 +77,9 @@ def timefunc(func: typing.Callable[P,T]) -> typing.Callable[P,T]:
         try:
             start_time = time.monotonic_ns()
             result = func(*args, **kwargs)
-            logger.debug(f"{func.__name__}({signature}) took: {(time.monotonic_ns() - start_time)/1_000_000_000} seconds") ## TODO (vijay): will this be logged if exception is raised in a long running function?
+            logger.debug(
+                f"{func.__name__}({signature}) took: {(time.monotonic_ns() - start_time) / 1_000_000_000} seconds"
+            )  ## TODO (vijay): will this be logged if exception is raised in a long running function?
             return result
         except Exception as e:
             logger.exception(f"Exception raised in {func.__name__}({signature}). exception: {e!s}")
@@ -78,54 +91,69 @@ def timefunc(func: typing.Callable[P,T]) -> typing.Callable[P,T]:
 def f1(**kwargs):
     print("key\tvalue")
     print("---\t-----")
-    for k,v in kwargs.items():
-        print(f"{k=}",f"{v=}",sep="\t")
+    for k, v in kwargs.items():
+        print(f"{k=}", f"{v=}", sep="\t")
 
-Num = typing.TypeVar('Num', int, float, complex)
-@log
-def sum(a:Num, b:Num) -> Num: return a+b
+
+Num = typing.TypeVar("Num", int, float, complex)
+
 
 @log
-def prod(a:Num, b:Num) -> Num: return a*b
+def sum(a: Num, b: Num) -> Num:
+    return a + b
+
+
+@log
+def prod(a: Num, b: Num) -> Num:
+    return a * b
+
 
 @log
 @hy.settings(max_examples=500, verbosity=hy.Verbosity.verbose)
-@hy.given(a=st.one_of(st.integers(), st.floats()), b=st.one_of(st.integers(), st.floats()))
-def test_sum[T: int|float](a:T, b:T) -> None:
-    """ This ought to fail for quite a few cases!!!
+@hy.given(
+    a=st.one_of(st.integers(), st.floats()), b=st.one_of(st.integers(), st.floats())
+)
+def test_sum[T: int | float](a: T, b: T) -> None:
+    """This ought to fail for quite a few cases!!!
 
-        Included here just to demonstrate how to use hypothesis for PBT.
+    Included here just to demonstrate how to use hypothesis for PBT.
     """
-    assert a + b == sum(a,b)
-    assert sum(a,b) >= a
-    assert sum(a,b) >= b
+    assert a + b == sum(a, b)
+    assert sum(a, b) >= a
+    assert sum(a, b) >= b
+
 
 @log
-@pytest.mark.parametrize(("test_input","expected"),[("3+5",8),("2+4",6),pytest.param("6*9",42,marks=pytest.mark.xfail)])
-def test_eval(test_input, expected): ## the args have to match args listed in the pytest.mark.parametrize tuple!
+@pytest.mark.parametrize(
+    ("test_input", "expected"),
+    [("3+5", 8), ("2+4", 6), pytest.param("6*9", 42, marks=pytest.mark.xfail)],
+)
+def test_eval(
+    test_input, expected
+):  ## the args have to match args listed in the pytest.mark.parametrize tuple!
     assert eval(test_input) == expected
+
 
 @log
 def main():
     logging.info("In main")
     print("hello world!")
-    f1(fname="vijay",lname="lulla",addr="mythical city",salary=5.2)
-    f1(x=[1,2,3,4,5],y=[1,2,3,4,5])
+    f1(fname="vijay", lname="lulla", addr="mythical city", salary=5.2)
+    f1(x=[1, 2, 3, 4, 5], y=[1, 2, 3, 4, 5])
     million, scaler = 1_000_000, 0.89
     if __debug__: pdb.set_trace()
-    print(f"${million:_d} US = \u20ac{million*scaler:4_.2f}")
+    print(f"${million:_d} US = \u20ac{million * scaler:4_.2f}")
     print(f"{sum(2,18.5)=}\n{prod(2,18.5)=}")
 
     today = datetime.datetime.now()
     print(f"{today:%B %d, %Y}\n"
           f"{today=:%B %d, %Y}\n"
           f"{ today = :%B %d, %Y}" ) ## NOTE: whitespace preserved!
+    print(stats.quantiles(range(1, 100), n=10))
+    year, films_totals = [1971, 1975, 1979, 1982, 1983], [1, 2, 3, 4, 5]
+    slope, intercept = stats.linear_regression(year, films_totals)
+    print(f"{round(slope * 2019 + intercept)}")
 
-    print(stats.quantiles(range(1,100),n=10))
-    year, films_totals = [1971,1975,1979,1982,1983],[1,2,3,4,5]
-    slope, intercept = stats.linear_regression(year,films_totals)
-    print(f"{round(slope*2019 + intercept)}")
 
 if __name__ == "__main__":
     main()
-
